e498156f6c88db442b32d328a007394ec111b75c
Christian Schaffner <67421480+technochicken@users.noreply.github.com>
2024-10-08 21:18:10 +0200

    Update convert.js

--- convert.js
+++ convert.js
@@ -79,11 +79,4 @@ class RALConverter {
 
 // Load the CSV data on page load
 loadRALData();
-
-// Example usage after loading the CSV:
-setTimeout(() => {
-    console.log(RALConverter.hexToRal('#CDBA88')); // { ral: "RAL 1000", name: "Green beige" }
-    console.log(RALConverter.ralToHex('RAL 1001')); // "#D0B084"
-    console.log(RALConverter.ralToName('RAL 1002')); // "Sand yellow"
-}, 2000);  // Delay added to ensure CSV is loaded
 </script>
